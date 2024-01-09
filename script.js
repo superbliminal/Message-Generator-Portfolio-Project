22024f451a7e6b832890148622908dc461918bfc
Paul <paulwhal@gmail.com>
2024-01-08 22:40:31 -0500

    added message generator function

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
-let willHave = ["fortune", "fame", "bad luck", "loss of a loved one", "significant change"];
+let willHave = ["fortune", "fame", "bad luck", "a loss of someone close", "significant change"];
 let timeFrame = ["this year", "next year", "soon", "at an unexpected time", "during the next full moon"];
 let followUp = ['Good luck!', 'Wow!', 'See ya!', "Anything else?", "Are you satisfied?"];
+
+const messageGenerator = () => {
+    let randWillHave = willHave[Math.floor(Math.random() * willHave.length)]
+    let randTimeFrame = timeFrame[Math.floor(Math.random() * timeFrame.length)]
+    let randFollowUp = followUp[Math.floor(Math.random() * followUp.length)]
+    console.log(`You will have ${randWillHave} ${randTimeFrame}. ${randFollowUp}`)
+}
+
+messageGenerator();
