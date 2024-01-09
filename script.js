6a12016b3815a12271118fdb4f4a359a48ee2509
Paul <paulwhal@gmail.com>
2024-01-08 22:14:53 -0500

    added blocks of message

---
+++ script.js
@@ -0,0 +1,3 @@
+let willHave = ["fortune", "fame", "bad luck", "loss of a loved one", "significant change"];
+let timeFrame = ["this year", "next year", "soon", "at an unexpected time", "during the next full moon"];
+let followUp = ['Good luck!', 'Wow!', 'See ya!', "Anything else?", "Are you satisfied?"];
